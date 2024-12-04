2f335096e0cd6388509c49536094bf918c333c4d
adityamishra139 <aditya2vibha@gmail.com>
2024-12-04 17:00:22 +0530

    sideboard disable

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom"; // Import Routes and Route
 import LandingPage from "./pages/Landingpage";
 import Dashboard from "./pages/Dashboard";
+import TaskDetails from "./pages/TaskDetails"
 
 const App = () => (
   <Routes>
     <Route path="/" element={<LandingPage />} />
     <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/task" element={<TaskDetails />} />
     {/* More routes can be added here */}
   </Routes>
 );
